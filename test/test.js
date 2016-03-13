bf9522e37dbe37ca0c6cc01939067fbc94b5c0a5
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-03-12 21:32:17 -0800

    started new codegen code

---
+++ test/test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    // expect = require('chai').expect,
+    jsof = require('jsof'),
+    codegen = require('../');
+
+var src = path.resolve(__dirname, '../wast-parser/results/');
+
+var astFileNames = fs.readdirSync(src);
+
+describe('codegen', function () {
+    astFileNames.forEach(function (name) {
+        it(name, function (done) {
+            if (name.match('^(.*)js$')) {
+                fs.readFile(
+                    path.resolve(src, name),
+                    'utf8',
+                    function (err, jsData) {
+                        var ast;
+                        if (err) { throw err; }
+                        ast = jsof.p(jsData);
+                        var res = codegen.generate(ast);
+                        console.log(res);
+                        done();
+                    }
+                );
+            } else {
+                done();
+            }
+        });
+    });
+});
