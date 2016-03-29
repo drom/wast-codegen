d570316f5f35a3cd97d18daa49b2c28421195362
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-03-29 08:28:02 -0700

    added default indent

--- bin/generator.js
+++ bin/generator.js
@@ -6,16 +6,20 @@ var spec = require('wast-spec'),
     esotope = require('esotope');
 
 var unparented = {
-    'identifier': true,
-    'literal': true
+    identifier: true,
+    literal: true,
+    script: true,
+    item: true
     // TODO add the rest
 };
 
 var compositeName = {
     binop: 'node.type + \'.\' + node.operator',
     cvtop: 'node.type + \'.\' + node.operator + \'/\' + node.type1',
-    const: 'node.type + \'.const\'',
-    identifier: '\'$\' + node.name'
+    const: 'node.type + \'.const \' + node.init',
+    identifier: '\'$\' + node.name',
+    item: '(node.name ? \'$\' + node.name : \'\') + node.type',
+    script: '\'(; ;)\''
     // TODO add the rest
 };
 
@@ -31,47 +35,42 @@ function parse (str) {
     return esprima.parse(str).body[0];
 }
 
-function accGen (right) {
-    return {
-        type: 'ExpressionStatement',
-        expression: {
-           type: 'AssignmentExpression',
-           operator: '+=',
-           left: { type: 'Identifier', name: 'res' },
-           right: right
-       }
-   };
-}
-
 function bodyGen (obj, kind) {
-    var res = [];
+    var objKinds = obj[kind],
+        res = [];
+    res.push(parse(`res += indent`));
     if (unparented[kind] === undefined) {
-        res.push(accGen({ type: 'Literal', value: '(' }));
+        res.push(parse(`res += '('`));
     }
     if (compositeName[kind] === undefined) {
-        res.push(accGen({ type: 'Literal', value: kind }));
+        res.push(parse(`res += '${kind}'`));
     } else {
-        res.push(accGen(parse(compositeName[kind]).expression));
+        res.push(parse('res += ' + compositeName[kind]));
     }
-    obj[kind].forEach(function (key) {
-        if (arrayKeys[key] === undefined) {
-            res.push(parse(
-                `if (node.${key}) {
-                    res += ' ';
-                    exprGen[node.${key}.kind](node.${key});
-                }`
-            ));
-        } else {
-            res.push(parse(
-                `node.${key}.forEach(function (e) {
-                    res += ' ';
-                    exprGen[e.kind](e);
-                });`
-            ));
+    if (objKinds.length > 0) {
+        res.push(parse(`indent += '  '`));
+        objKinds.forEach(function (key) {
+            if (arrayKeys[key] === undefined) {
+                res.push(parse(
+                    `if (node.${key}) {
+                        exprGen[node.${key}.kind](node.${key});
+                    }`
+                ));
+            } else {
+                res.push(parse(
+                    `node.${key}.forEach(function (e) {
+                        exprGen[e.kind](e);
+                    });`
+                ));
+            }
+        });
+        res.push(parse(`indent = indent.slice(0,-2)`));
+        if (unparented[kind] === undefined) {
+            res.push(parse(`res += indent`));
         }
-    });
+    }
     if (unparented[kind] === undefined) {
-        res.push(accGen({ type: 'Literal', value: ')' }));
+        res.push(parse(`res += ')'`));
     }
     return res;
 }
@@ -79,10 +78,11 @@ function bodyGen (obj, kind) {
 function funcObject (obj) {
     var res = esprima.parse(`
         'use strict';
-        var res;
+        var res, indent;
         var exprGen = {};
         function gen (node) {
             res = '';
+            indent = '\\n';
             exprGen[node.kind](node);
             return res;
         }
