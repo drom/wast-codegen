8f0c454dd6c731bc8f8c7eddd3062e87bf5026da
wanderer <mjbecze@gmail.com>
2016-03-30 11:01:10 -0400

    added test for blocks

--- test/basic.js
+++ test/basic.js
@@ -109,4 +109,56 @@ describe('basic', function () {
         expect(result).to.eq(wast);
         done();
     });
+
+    it.only('blocks', function (done){
+        var wast =
+`(module
+  (func
+    (block $switch)))`
+
+        var json = parser.parse(wast);
+        var result = lib.generate(json, 2);
+        expect(result).to.eq(wast);
+        done();
+
+    });
 });
+
+// var wast =
+// `(module
+//   (func $stmt
+//     (param $i i32)
+//     (result i32)
+//     (local $j i32)
+//     (set_local $j
+//       (i32.const 100))
+//     (block $switch
+//       (block $7
+//         (block $default
+//           (block $6
+//             (block $5
+//               (block $4
+//                 (block $3
+//                   (block $2
+//                     (block $1
+//                       (block $0
+//                         (br_table $0 $1 $2 $3 $4 $5 $6 $7 $default
+//                           (get_local $i)))
+//                       (return (get_local $i)))
+//                     (nop)))
+//                 (set_local $j
+//                   (i32.sub
+//                     (i32.const 0)
+//                     (get_local $i)))
+//                 (br $switch))
+//               (br $switch))
+//             (set_local $j
+//               (i32.const 101))
+//             (br $switch))
+//           (set_local $j
+//             (i32.const 101)))
+//         (set_local $j
+//           (i32.const 102))))
+//     (return
+//       (get_local $j))))`
+
